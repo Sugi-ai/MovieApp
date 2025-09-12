06549ec56ec341bf8661e8422eb33b7bf9c22feb
Sugi-ai <suhdei90@gmail.com>
2025-09-12 09:59:19 +0800

    page 1 footer heseg l dutuu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
-import { ThemeProvider } from "@/components/home/theme-provider";
 import type { Metadata } from "next";
-
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/main/Header";
+import { ThemeProvider } from "@/components/home/theme-provider";
+import { Header } from "@/components/main";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
